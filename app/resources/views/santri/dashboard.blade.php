119d1d8e9d4d461b58d62a124004a93792ac2526
Adyatmaa <adyatmaa001@gmail.com>
2024-06-13 12:34:07 +0700

    syncing

--- app/resources/views/santri/dashboard.blade.php
+++ app/resources/views/santri/dashboard.blade.php
@@ -13,14 +13,14 @@
             {{-- Card Absensi --}}
             <div
                 class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
-                <h5 class="mb-4 text-base font-medium tracking-tight text-gray-900 text-start">Senin • 21 Januari</h5>
+                <h5 class="mb-4 text-base font-medium tracking-tight text-gray-900 text-start">Senin</h5>
                 <div class="mb-4">
                     <!-- Modal toggle -->
                     <button data-modal-target="crud-modal" data-modal-toggle="crud-modal"
                         class="block text-white bg-emerald-200 hover:bg-emerald-300 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 w-full text-left"
                         type="button">
                         <h4 class="text-base font-medium text-slate-700">Taklim Afkar</h4>
-                        <p class="text-xs font-light text-slate-700 mb-3">18.00-21.00</p>
+                        <p class="text-xs font-light text-slate-700 mb-3">18.00</p>
                         <h4 class="text-sm font-normal text-slate-700">Ustad Abid Alijaber</h4>
                     </button>
 
@@ -87,7 +87,7 @@
                         class="block text-white bg-orange-200 hover:bg-orange-300 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 w-full text-left"
                         type="button">
                         <h4 class="text-base font-medium text-slate-700">Taklim Afkar</h4>
-                        <p class="text-xs font-light text-slate-700 mb-3">18.00-21.00</p>
+                        <p class="text-xs font-light text-slate-700 mb-3">18.00</p>
                         <h4 class="text-sm font-normal text-slate-700">Ustad Abid Alijaber</h4>
                     </button>
 
@@ -153,14 +153,14 @@
             {{-- Card Absensi --}}
             <div
                 class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <h5 class="mb-4 text-base font-medium tracking-tight text-gray-900">Senin • 21 Januari</h5>
+                <h5 class="mb-4 text-base font-medium tracking-tight text-gray-900">Senin</h5>
                 <div class="mb-4">
                     <!-- Modal toggle -->
                     <button data-modal-target="crud-modal" data-modal-toggle="crud-modal"
                         class="block text-white bg-emerald-200 hover:bg-emerald-300 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 w-full text-left"
                         type="button">
                         <h4 class="text-base font-medium text-slate-700">Taklim Afkar</h4>
-                        <p class="text-xs font-light text-slate-700 mb-3">18.00-21.00</p>
+                        <p class="text-xs font-light text-slate-700 mb-3">18.00</p>
                         <h4 class="text-sm font-normal text-slate-700">Ustad Abid Alijaber</h4>
                     </button>
 
@@ -227,7 +227,7 @@
                         class="block text-white bg-orange-200 hover:bg-orange-300 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 w-full text-left"
                         type="button">
                         <h4 class="text-base font-medium text-slate-700">Taklim Afkar</h4>
-                        <p class="text-xs font-light text-slate-700 mb-3">18.00-21.00</p>
+                        <p class="text-xs font-light text-slate-700 mb-3">18.00</p>
                         <h4 class="text-sm font-normal text-slate-700">Ustad Abid Alijaber</h4>
                     </button>
 
@@ -293,14 +293,14 @@
             {{-- Card Absensi --}}
             <div
                 class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <h5 class="mb-4 text-base font-medium tracking-tight text-gray-900">Senin • 21 Januari</h5>
+                <h5 class="mb-4 text-base font-medium tracking-tight text-gray-900">Senin</h5>
                 <div class="mb-4">
                     <!-- Modal toggle -->
                     <button data-modal-target="crud-modal" data-modal-toggle="crud-modal"
                         class="block text-white bg-emerald-200 hover:bg-emerald-300 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 w-full text-left"
                         type="button">
                         <h4 class="text-base font-medium text-slate-700">Taklim Afkar</h4>
-                        <p class="text-xs font-light text-slate-700 mb-3">18.00-21.00</p>
+                        <p class="text-xs font-light text-slate-700 mb-3">18.00</p>
                         <h4 class="text-sm font-normal text-slate-700">Ustad Abid Alijaber</h4>
                     </button>
 
@@ -367,7 +367,7 @@
                         class="block text-white bg-orange-200 hover:bg-orange-300 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 w-full text-left"
                         type="button">
                         <h4 class="text-base font-medium text-slate-700">Taklim Afkar</h4>
-                        <p class="text-xs font-light text-slate-700 mb-3">18.00-21.00</p>
+                        <p class="text-xs font-light text-slate-700 mb-3">18.00</p>
                         <h4 class="text-sm font-normal text-slate-700">Ustad Abid Alijaber</h4>
                     </button>
 
